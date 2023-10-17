7630fdfdf87398360591d28d22b5862c2e537238
bpk667 <pedro.pozuelo@gmail.com>
2023-10-17 21:42:30 +0200

    First version

---
+++ Readme.md
@@ -0,0 +1,53 @@
+## Goal
+Get the flag for each exercice.
+
+If it is not a challenge:
+ - Try to get the flag without executing anything. (DON'T TRUST THE CODE).
+ - Get a one liner without chaining commands (no `&&`, `||` or similar).
+
+# CTF exercises
+
+## 1. Test terminal: `wget` and Internet/lab access (pg. 55)
+
+Entry point: https://linuxctf.ddns.net/ 
+
+## 2. Base64 and pipes. (pg. 68)
+
+Entry point: https://drive.proton.me/urls/T034ZTH94R#oVZmVcn177gD
+
+## 3. Iterate with `wget` or `curl`. (pg. 79)
+
+Entry point: https://linuxctf.ddns.net/shop.html
+
+## 4. runme (pg. 91)
+
+Entry point: https://drive.proton.me/urls/S5Y8ZD0ZMC#mnu0o3VLiXyR
+
+## 5. Recursive download (pg. 102)
+Entry point: https://linuxctf.ddns.net/recursive/
+
+## 6. Command tar and process result (pg. 108)
+
+### 6.1. Commands tar and find.
+
+Entry point: https://drive.proton.me/urls/KJW0CY6XG8#HeHKCBETLQAM
+
+### 6.2. Commands tar & GPG (plus install GPG if necessary)
+
+Entry point: https://drive.proton.me/urls/88EC74QF2W#xpqN3vYIWgta
+
+## 7. Install python dependencies (pg. 148)
+
+Entry point: https://drive.proton.me/urls/9VNBR2A0G0#R8j8UuIiXhTY
+
+## 8. Docker flags (pg. 164)
+
+There are 3 flags (`docker_flag`) in the following image.
+```
+docker pull pedropozuelo/linuxctf
+```
+
+# Extra ball. Can you cover your creds?
+
+Fix the image so it doesn't expose the secrets.
+
---
+++ exercise2/hex2str.py
@@ -0,0 +1,45 @@
+import sys
+import base64
+import random
+import string
+
+usage_msg = "Usage: "+ sys.argv[0] +" (-d/-h)"
+help_msg = usage_msg + "\n" +\
+        "Examples:\n" +\
+        "  To decode the file 'file.txt' (in hexadecimal format), do:\n  $ cat 'file.txt' |python "+ sys.argv[0] +" -d\n"
+
+def isBase64(s):
+    try:
+        base64.b64encode(base64.b64decode(s)) == s
+        print ('Sorry, the base64 module is broken. Decode it before calling the script')
+        exit (-1)
+    except Exception:
+        return False
+
+
+if len(sys.argv) != 2:
+    print(usage_msg)
+    sys.exit(1)
+
+if sys.argv[1] == "-d":
+    data = input()
+    if not isBase64(data):
+        try:
+            data = bytes.fromhex(data).decode('utf-8')
+            for c in data:
+                print(''.join(random.choice(string.ascii_lowercase) for i in range(20)), file=sys.stderr, end="", flush=False)
+                print(c, end="", flush=True)
+                print(''.join(random.choice(string.ascii_lowercase) for i in range(20)) + " ", file=sys.stderr, flush=True)
+            print()
+            print ("\nOutput can be a mess. Maybe you want to filter the standard error", file=sys.stderr)
+        except Exception:
+            print('Sorry. Could not decode the string. Is it in hexadecimal format?')
+            sys.exit(1)
+
+elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    print(help_msg)
+    sys.exit(1)
+
+else:
+    print("Unrecognized first argument: "+ sys.argv[1])
+    print("Please use '-d', or '-h'.")
